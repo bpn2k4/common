75618da9c135d3ce64ccc4b100bbaa9ce14f9c1a
Bui Phuong Nam <121207859+bpn2k4@users.noreply.github.com>
2024-05-09 21:10:40 +0700

    update script start container

--- vscode/start.sh
+++ vscode/start.sh
@@ -2,4 +2,4 @@
 
 sudo chown -R ubuntu:ubuntu /home/ubuntu/workspace
 
-/home/ubuntu/.local/bin/code-server/bin/code-server --host 0.0.0.0 --port 8080 --auth none --user-data-dir /home/ubuntu
+code-server --host 0.0.0.0 --port 8080 --auth none --user-data-dir /home/ubuntu
