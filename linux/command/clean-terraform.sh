620dba96474eeb9c8afe4537bf4415ab318b738d
Bui Phuong Nam <bpn2k4@gmail.com>
2026-07-24 15:31:46 +0700

    add command to clean terraform lock

--- linux/command/clean-terraform.sh
+++ linux/command/clean-terraform.sh
@@ -1,3 +1,7 @@
 find . -type d -name ".terraform"
 
 find . -type d -name ".terraform" -prune -exec rm -rf {} +
+
+find . -type f -name ".terraform.lock.hcl"
+
+find . -type f -name ".terraform.lock.hcl" -prune -exec rm -rf {} +
